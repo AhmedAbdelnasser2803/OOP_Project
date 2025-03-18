c138df1b6586db4e0aff5d48c243d64e6c8807a0
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-18 13:07:00 +0200

    static method don't nedd object from the class

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -2,5 +2,13 @@ package firstPackage;
 
 public class FirstClass
 {
-
+    //simple methiod to print the words
+    public static void  printLine()
+    {
+        System.out.println("hello from print line method");
+    }
+    public static void printLine2(String x)
+    {
+        System.out.println(x);
+    }
 }
--- src/firstPackage/SecondClass.java
+++ src/firstPackage/SecondClass.java
@@ -1,4 +1,13 @@
 package firstPackage;
 
 public class SecondClass {
+    public static void main (String [] args)
+    {
+        // i can use the class name direct to access static method of the class
+        //videos 27
+        FirstClass.printLine();
+        FirstClass.printLine2("ahmed abdelnasser ");
+
+    }
+
 }
