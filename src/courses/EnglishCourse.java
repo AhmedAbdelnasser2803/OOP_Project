0e4135cfb788f015ff44a55e40afc15e2daa8d7e
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-19 13:53:13 +0200

    declare class or static var

--- src/courses/EnglishCourse.java
+++ src/courses/EnglishCourse.java
@@ -3,15 +3,15 @@ package courses;
 //ClassName
 public class EnglishCourse {
 
-    private int age =60;
-    public int testInstanceVar()
+    static int age =60;
+    public static int testInstanceVar()
     {
         return age;
     }
     public static void main(String [] args)
     {
-        EnglishCourse course = new EnglishCourse();
-        System.out.println(course.testInstanceVar());
+
+        System.out.println(testInstanceVar());
     }
 
 }
