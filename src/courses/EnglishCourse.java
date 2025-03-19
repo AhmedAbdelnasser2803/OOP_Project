8b4b100feade33523b123a9e7a8dcb55a5d14c96
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-19 14:21:36 +0200

    declare the String and get the length of this string

--- src/courses/EnglishCourse.java
+++ src/courses/EnglishCourse.java
@@ -3,15 +3,14 @@ package courses;
 //ClassName
 public class EnglishCourse {
 
-    static int age =60;
-    public static int testInstanceVar()
-    {
-        return age;
-    }
     public static void main(String [] args)
     {
+        //declare string and get the length of the string and use concatenate
+        String name = "ahmed abdelnasser saad";
+        System.out.println("the name is : "+name);
+        //get the length of the string
+        System.out.println("the length is "+name.length());
 
-        System.out.println(testInstanceVar());
     }
 
 }
