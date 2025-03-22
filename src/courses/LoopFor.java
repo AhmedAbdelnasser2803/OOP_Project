f40cd8d618051513f8c7610aa580c58104253f38
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-22 19:43:26 +0200

    for loop examplein LoopFor class

---
+++ src/courses/LoopFor.java
@@ -0,0 +1,10 @@
+package courses;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++)
+        {
+            System.out.println("the value of iteration is "+(i+1));
+        }
+    }
+}
