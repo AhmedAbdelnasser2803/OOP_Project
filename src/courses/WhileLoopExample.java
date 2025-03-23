1784ad04d264d236f9672a665c74c6a29d870528
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-23 08:22:11 +0200

    while loop example

---
+++ src/courses/WhileLoopExample.java
@@ -0,0 +1,14 @@
+package courses;
+
+public class WhileLoopExample {
+    //we loop to iteration until the condition be false
+    public static void main(String[] args) {
+        int i =0;
+        while(i<=5)
+        {
+            System.out.println("the value is : "+i);
+            i++; // that to increment the value of variable like for
+
+        }
+    }
+}
