9baf26d8923ed5bff4eb7d2321ba2b77aef6ce2c
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-23 08:28:59 +0200

    do while loop example

---
+++ src/courses/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package courses;
+
+public class DoWhileLoop {
+    //this statement execute the statement at lest one time
+    public static void main(String[] args) {
+        int i =7;
+        do {
+            System.out.println("the value is :"+i);
+            i++;
+        }while (i<5);
+    }
+}
