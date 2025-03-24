dd8d2e572a5b442c2dc4822a07c2a6bc6b0b1616
ahmed abdelnasser <ahmed.abdelnasser2803@gmail.com>
2025-03-24 11:15:07 +0200

    two type of exeption

---
+++ src/courses/FileNotFoundDemo.java
@@ -0,0 +1,32 @@
+package courses;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileNotFoundDemo {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
+        * that is the type of exception
+        * if the file not found
+        * */
+//        File file = new File("c://file.txt");
+//        FileReader fr = new FileReader(file);
+//        System.out.println("hello");
+        /*
+        * the second type of exception
+        * if i select the index of array which is out of bound
+        * */
+        FileNotFoundDemo obj1 = new FileNotFoundDemo();
+        obj1.TestArray();
+
+    }
+    public void TestArray()
+    {
+        int arr1 [] = {1,2,3,4,5,6};
+        System.out.println("the value is "+arr1[7]);
+    }
+
+}
